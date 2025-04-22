148cfc197454c3ed9d4fc9f7245c50a300c30077
saikumar pillalamarri <pillalamarrisaikumar8@gmail.com>
2025-04-22 14:02:21 +0530

    HeroSection

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import HeroSection from './Components/HeroSection/HeroSection';
 import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <HeroSection />
     </div>
   );
 }

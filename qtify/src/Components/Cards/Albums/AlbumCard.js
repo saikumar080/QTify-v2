d0eb3d89a9ee2cc1da38d2fcddb51a6678d3de03
saikumar pillalamarri <pillalamarrisaikumar8@gmail.com>
2025-06-28 13:05:02 +0530

    Album Grid

--- qtify/src/Components/Cards/Albums/AlbumCard.js
+++ qtify/src/Components/Cards/Albums/AlbumCard.js
@@ -2,26 +2,47 @@ import React from "react";
 import styles from './AlbumCard.module.css';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
-// import CardContent from '@mui/material/CardContent';
 import Chip from '@mui/material/Chip';
 import Typography from '@mui/material/Typography';
+import { Grid } from "@mui/material";
 const AlbumCard=({image, name, follows})=>{
     return(
        <div className={styles.wrapper}>
         <Card className={styles.card} elevation={2}>
-            <CardMedia
-                component="img"
-                image={image}
-                alt={name}
-                className={styles.image}
-            />
-                <Chip
-                label={`${follows} Follows`} 
-                className={styles.chip}
-                size="small"
-                 sx={{ position:'absolute',bottom:8, left:8, backgroundColor: '#000000', color: '#FFFFFF', fontSize:'0.7rem',padding:'0 6px', borderRadius:'4px',fontWeight:500}}
-                />
-            </Card>    
+            <Grid container direction="column">
+                <Grid item>
+                    <CardMedia
+                        component="img"
+                        image={image}
+                        alt={name}
+                        className={styles.image}
+                        sx={{
+                            borderTopLeftRadius: "8px",
+                            borderTopRightRadius: "8px",
+                        }}
+                    />
+                </Grid>
+                <Grid item
+                    sx={{
+                        backgroundColor:"#ffffff",
+                        padding:"8px",
+                        display:"flex",
+                        justifyContent:"flex-start",
+                        alignItems:"center",
+                        borderBottomLeftRadius:"8px",
+                        borderBottomRightRadius:"8px",
+                    }}>
+                        <Chip
+                            label={`${follows} Follows`} 
+                            className={styles.chip}
+                            size="small"
+                            sx={{ backgroundColor: '#000000', color: '#FFFFFF', fontSize:'0.7rem',padding:'0 6px', borderRadius:'4px',fontWeight:500}}
+                        />  
+                </Grid>
+            </Grid>
+
+            </Card> 
+           
             <Typography variant="subtitle1" className={styles.albumName}>
                 {name}
             </Typography>
